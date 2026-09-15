063fa71d48e9ab17f5a15fcf2b114b882e1506e4
ysh011130 <78461483+ysh011130@users.noreply.github.com>
2026-09-15 11:09:17 +0900

    [Passed] 정수 선언하고 뺄셈 (92ms, 9MB)

---
+++ trail1/정수 선언하고 뺄셈/define-numbers-and-substract.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int x = 97;
+        int y = 13;
+        System.out.printf("%d - %d = %d", x, y, x-y);
+    }
+}
