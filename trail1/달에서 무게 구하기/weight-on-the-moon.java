f8c48a0e66c5bd9081c2f29ee8c2b4970b4f8404
ysh011130 <78461483+ysh011130@users.noreply.github.com>
2026-09-20 13:54:58 +0900

    [Passed] 달에서 무게 구하기 (94ms, 9MB)

---
+++ trail1/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int weight = 13;
+        double g = 0.165;
+
+        System.out.printf("%d * %f = %f", weight, g, weight * g);
+    }
+}
