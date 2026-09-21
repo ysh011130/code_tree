53182ae96dec1b997441d9470d167a13b4f47fcf
ysh011130 <78461483+ysh011130@users.noreply.github.com>
2026-09-21 13:37:27 +0900

    [Passed] 정수 복사 (93ms, 9MB)

---
+++ trail1/정수 복사/copy-integer.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 3;
+        int b = 4;
+        b = a;
+
+        System.out.println(a + " " + b);
+        System.out.println(a * b);
+    }
+}
