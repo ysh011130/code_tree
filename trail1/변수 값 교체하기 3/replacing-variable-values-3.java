645a065579e21684e45344f769140ec3f44a2b76
ysh011130 <78461483+ysh011130@users.noreply.github.com>
2026-09-22 17:56:03 +0900

    [Passed] 변수 값 교체하기 3 (107ms, 9MB)

---
+++ trail1/변수 값 교체하기 3/replacing-variable-values-3.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 3;
+        int b = 5;
+        int tmp;
+
+        tmp = a;
+        a = b;
+        b = tmp;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
